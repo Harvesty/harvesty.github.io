4e3786c859c72f307971d5694d728ec8d6804636
Harvesty <sohoearny@gmail.com>
2017-04-11 17:07:27 +0800

    addtest

---
+++ js/savetest.js
@@ -0,0 +1,34 @@
+(function(view) {
+"use strict";
+var
+	  document = view.document
+	, $ = function(id) {
+		return document.getElementById(id);
+	}
+	, session = view.sessionStorage
+	, txtCode = $("txtCode")
+	, frmQuestions = $("frmQuestions")
+	, txtFileName = $("txtFileName")
+
+  if (session.code) {
+	txtCode.value = session.txtCode;
+} if (session.txtFileName) {
+	txtFileName.value = session.txtFileName;
+}
+
+frmQuestions.addEventListener("submit", function(event) {
+	event.preventDefault();
+	saveAs(
+		  new Blob(
+			  [txtCode.value || txtCode.placeholder]
+			, {type: "application/json;charset=" + document.characterSet}
+		)
+		, (txtFileName.value || txtFileName.placeholder) + ".json"
+	);
+}, false);
+
+view.addEventListener("unload", function() {
+	session.txtCode = txtCode.value;
+	session.txtFileName = txtFileName.value;
+}, false);
+}(self));
