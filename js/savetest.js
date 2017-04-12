cda61b469a8cde44e41fe68a1ec57743b56baac5
Harvesty <sohoearny@gmail.com>
2017-04-12 17:43:06 +0800

    fix

--- js/savetest.js
+++ js/savetest.js
@@ -9,9 +9,29 @@
 		txtCode = $("txtCode"),
 		txtTest = $("txtTest"),
 		frmQuestions = $("frmQuestions"),
-		txtFileName = $("txtFileName")
+		txtFileName = $("txtFileName"),
+		pmGetData = getJSON("data/base.json");
 
-		;
+	function getJSON(url) {
+		return new Promise(function (resolve, reject) {
+
+			var request = new XMLHttpRequest();
+			request.open('GET', url);
+			request.responseType = 'json';
+
+			request.onload = function () {
+				if (request.status === 200) {
+					resolve(request.response);
+				} else {
+					reject(Error('JSON didn\'t load successfully; error code:' + request.statusText));
+				}
+			};
+			request.onerror = function () {
+				reject(Error('There was a network error.'));
+			};
+			request.send();
+		});
+	}
 
 	if (session.code) {
 		txtCode.value = session.txtCode;
@@ -22,6 +42,13 @@
 
 	frmQuestions.addEventListener("submit", function (event) {
 		event.preventDefault();
+		pmGetData.then(function (tests) {
+
+			tests.push({desc:'test',code:'code'});
+
+		}, function (Error) {
+			console.log(Error);
+		});
 		saveAs(
 			new Blob(
 				[txtCode.value || txtCode.placeholder], {
@@ -29,11 +56,12 @@
 				}
 			), (txtFileName.value || txtFileName.placeholder) + ".json"
 		);
+
 	}, false);
 
 	view.addEventListener("unload", function () {
 		session.txtCode = txtCode.value;
 		session.txtFileName = txtFileName.value;
 	}, false);
-	
+
 }(self));
