43d9f9a00ef356c7748c8a2651d95b4561a24012
Harvesty <sohoearny@gmail.com>
2017-04-12 09:07:05 +0800

    format

--- js/savetest.js
+++ js/savetest.js
@@ -1,34 +1,36 @@
-(function(view) {
-"use strict";
-var
-	  document = view.document
-	, $ = function(id) {
-		return document.getElementById(id);
+(function (view) {
+	"use strict";
+	var
+		document = view.document,
+		$ = function (id) {
+			return document.getElementById(id);
+		},
+		session = view.sessionStorage,
+		txtCode = $("txtCode"),
+		frmQuestions = $("frmQuestions"),
+		txtFileName = $("txtFileName")
+
+	if (session.code) {
+		txtCode.value = session.txtCode;
 	}
-	, session = view.sessionStorage
-	, txtCode = $("txtCode")
-	, frmQuestions = $("frmQuestions")
-	, txtFileName = $("txtFileName")
+	if (session.txtFileName) {
+		txtFileName.value = session.txtFileName;
+	}
 
-  if (session.code) {
-	txtCode.value = session.txtCode;
-} if (session.txtFileName) {
-	txtFileName.value = session.txtFileName;
-}
+	frmQuestions.addEventListener("submit", function (event) {
+		event.preventDefault();
+		saveAs(
+			new Blob(
+				[txtCode.value || txtCode.placeholder], {
+					type: "application/json;charset=" + document.characterSet
+				}
+			), (txtFileName.value || txtFileName.placeholder) + ".json"
+		);
+	}, false);
 
-frmQuestions.addEventListener("submit", function(event) {
-	event.preventDefault();
-	saveAs(
-		  new Blob(
-			  [txtCode.value || txtCode.placeholder]
-			, {type: "application/json;charset=" + document.characterSet}
-		)
-		, (txtFileName.value || txtFileName.placeholder) + ".json"
-	);
-}, false);
-
-view.addEventListener("unload", function() {
-	session.txtCode = txtCode.value;
-	session.txtFileName = txtFileName.value;
-}, false);
+	view.addEventListener("unload", function () {
+		session.txtCode = txtCode.value;
+		session.txtFileName = txtFileName.value;
+	}, false);
+	
 }(self));
