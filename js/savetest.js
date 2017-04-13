d8cbca3936d6a5702b3e62466a244a79b58255d7
Harvesty <sohoearny@gmail.com>
2017-04-13 15:16:41 +0800

    fix

--- js/savetest.js
+++ js/savetest.js
@@ -6,11 +6,13 @@
 			return document.getElementById(id);
 		},
 		session = view.sessionStorage,
+		uTests = null, // Unmodified
+		mTests = null, // Modified
+		btnAdd = $("btnAdd"),
 		txtCode = $("txtCode"),
 		txtTest = $("txtTest"),
 		frmQuestions = $("frmQuestions"),
-		txtFileName = $("txtFileName"),
-		pmGetData = getJSON("../data/base.json");
+		txtFileName = $("txtFileName");
 
 	function getJSON(url) {
 		return new Promise(function (resolve, reject) {
@@ -23,7 +25,7 @@
 				if (request.status === 200) {
 					resolve(request.response);
 				} else {
-					reject(Error('JSON didn\'t load successfully; error code:' + request.statusText));
+					reject(Error("JSON didn't load successfully; error code:" + request.statusText));
 				}
 			};
 			request.onerror = function () {
@@ -39,6 +41,41 @@
 	if (session.txtFileName) {
 		txtFileName.value = session.txtFileName;
 	}
+	getJSON("../data/base.json").then(function (tests) {
+
+		txtTest.value = tests[0].Q.desc;
+		txtCode.value = tests[0].Q.code;
+
+		let test = {
+			Q: {
+				desc: txtTest.value,
+				code: txtCode.value
+			},
+			A: {
+				desc: "",
+				code: ""
+			}
+		};
+		tests.push(test);
+		saveAs(
+			new Blob(
+				[JSON.stringify(tests)], {
+					type: "application/json;charset=" + document.characterSet
+				}
+			), (txtFileName.value || txtFileName.placeholder) + ".json"
+		);
+
+	}, function (Error) {
+		console.log(Error);
+	});
+
+	// 添加试题
+	btnAdd.addEventListener("click", function (event) {
+		event.preventDefault();
+
+		pmGetData
+
+	}, false);
 
 	frmQuestions.addEventListener("submit", function (event) {
 		event.preventDefault();
